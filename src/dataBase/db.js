fc661831cc7264362f3b250497010f06236dd38a
Dan315 <daniel827874735@gmail.com>
2022-12-08 18:42:41 -0600

    ajuste variables de prueba 3

--- src/dataBase/db.js
+++ src/dataBase/db.js
@@ -7,5 +7,13 @@ const users=[
     {nombre:"Paco",apellido:"Fernandez"},
     {nombre:"Daniel",apellido:"Romo"},
     {nombre:"David",apellido:"Ulloa"},
+    {nombre:"Daniel",apellido:"Romo"},
+    {nombre:"David",apellido:"Ulloa"},
+    {nombre:"Eduardo",apellido:"Perez"},
+    {nombre:"Paco",apellido:"Fernandez"},
+    {nombre:"Eduardo",apellido:"Perez"},
+    {nombre:"Paco",apellido:"Fernandez"},
+    {nombre:"Daniel",apellido:"Romo"},
+    {nombre:"David",apellido:"Ulloa"},
 ]
 module.exports = users;
