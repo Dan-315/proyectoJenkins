5e35a0cba32e989149baf9a1d126b9e95ef1f44a
Dan315 <daniel827874735@gmail.com>
2022-12-08 19:34:04 -0600

    Ajuste en variables de pruebas 4

--- src/dataBase/db.js
+++ src/dataBase/db.js
@@ -3,17 +3,5 @@ const users=[
     {nombre:"David",apellido:"Ulloa"},
     {nombre:"Eduardo",apellido:"Perez"},
     {nombre:"Paco",apellido:"Fernandez"},
-    {nombre:"Eduardo",apellido:"Perez"},
-    {nombre:"Paco",apellido:"Fernandez"},
-    {nombre:"Daniel",apellido:"Romo"},
-    {nombre:"David",apellido:"Ulloa"},
-    {nombre:"Daniel",apellido:"Romo"},
-    {nombre:"David",apellido:"Ulloa"},
-    {nombre:"Eduardo",apellido:"Perez"},
-    {nombre:"Paco",apellido:"Fernandez"},
-    {nombre:"Eduardo",apellido:"Perez"},
-    {nombre:"Paco",apellido:"Fernandez"},
-    {nombre:"Daniel",apellido:"Romo"},
-    {nombre:"David",apellido:"Ulloa"},
 ]
 module.exports = users;
